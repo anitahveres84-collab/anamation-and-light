4894b9bfcf788dce910a6381dbb05275e0a10272
anitahveres84-collab <anitahveres84@gmail.com>
2026-07-01 16:42:55 +0100

    Make it 70 % Changes Today

--- main.ts
+++ main.ts
@@ -1,20 +1,134 @@
-scene.setBackgroundColor(13)
+scene.setBackgroundColor(7)
 let Player_1 = sprites.create(img`
-    . . . . . . . . . . b b b . . . 
-    . . . . . . . . b e e 3 3 b . . 
-    . . . . . . b b e 3 2 e 3 a . . 
-    . . . . b b 3 3 e 2 2 e 3 3 a . 
-    . . b b 3 3 3 3 3 e e 3 3 3 a . 
-    b b 3 3 3 3 3 3 3 3 3 3 3 3 3 a 
-    b 3 3 3 d d d d 3 3 3 3 3 d d a 
-    b b b b b b b 3 d d d d d d 3 a 
-    b d 5 5 5 5 d b b b a a a a a a 
-    b 3 d d 5 5 5 5 5 5 5 d d d d a 
-    b 3 3 3 3 3 3 d 5 5 5 d d d d a 
-    b 3 d 5 5 5 3 3 3 3 3 3 b b b a 
-    b b b 3 d 5 5 5 5 5 5 5 d d b a 
-    . . . b b b 3 d 5 5 5 5 d d 3 a 
-    . . . . . . b b b b 3 d d d b a 
-    . . . . . . . . . . b b b 3 a . 
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b c . . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    b d d d b b d 5 5 5 4 4 4 4 4 b 
+    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+    c d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c b d d c c b 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
     `, SpriteKind.Player)
 controller.moveSprite(Player_1, 100, 100)
+animation.runImageAnimation(
+Player_1,
+[img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . b 5 5 b . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . b b b b b 5 5 5 5 5 5 5 b . . 
+    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+    . . b d 5 5 b 1 f f 5 4 4 c . . 
+    b b d b 5 5 5 d f b 4 4 4 4 b . 
+    b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . b b b b b 5 5 5 5 5 5 5 b . . 
+    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+    . . b d 5 5 b 1 f f 5 4 4 c . . 
+    b b d b 5 5 5 d f b 4 4 4 4 4 b 
+    b d d c d 5 5 b 5 4 4 4 4 4 b . 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `,img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b c . . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    b d d d b b d 5 5 5 4 4 4 4 4 b 
+    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+    c d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c b d d c c b 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `,img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 d 4 c . . 
+    . . . . b 5 5 1 f f d d 4 4 4 b 
+    . . . . b 5 5 d f b 4 4 4 4 b . 
+    . . . b d 5 5 5 5 4 4 4 4 b . . 
+    . . b d d 5 5 5 5 5 5 5 5 b . . 
+    . b d d d d 5 5 5 5 5 5 5 5 b . 
+    b d d d b b b 5 5 5 5 5 5 5 b . 
+    c d d b 5 5 d c 5 5 5 5 5 5 b . 
+    c b b d 5 d c d 5 5 5 5 5 5 b . 
+    . b 5 5 b c d d 5 5 5 5 5 d b . 
+    b b c c c d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `,img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 d 4 c . . 
+    . . . . b 5 5 1 f f d d 4 4 4 b 
+    . . . . b 5 5 d f b 4 4 4 4 b . 
+    . . . b d 5 5 5 5 4 4 4 4 b . . 
+    . b b d d d 5 5 5 5 5 5 5 b . . 
+    b d d d b b b 5 5 5 5 5 5 5 b . 
+    c d d b 5 5 d c 5 5 5 5 5 5 b . 
+    c b b d 5 d c d 5 5 5 5 5 5 b . 
+    c b 5 5 b c d d 5 5 5 5 5 5 b . 
+    b b c c c d d d 5 5 5 5 5 d b . 
+    . . . . c c d d d 5 5 5 b b . . 
+    . . . . . . c c c c c b b . . . 
+    `,img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+    b d d d b b d 5 5 4 4 4 4 4 b . 
+    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+    c b d c d 5 5 b 5 5 5 5 5 5 b . 
+    . c d d c c b d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    . . . . . . . . . . . . . . . . 
+    `],
+50,
+true
+)
+animation.runMovementAnimation(
+Player_1,
+animation.animationPresets(animation.flyToCenter),
+100,
+false
+)
